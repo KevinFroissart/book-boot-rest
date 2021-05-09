c263476c6b9bf26eb4ac690d1f1148876e68bb51
KevinFroissart <kevin.froissart69@gmail.com>
2021-05-09 16:44:37 +0200

    the whole actual thing

---
+++ src/main/java/com/kasket/bootrestapi/Book.java
@@ -0,0 +1,32 @@
+package com.kasket.bootrestapi;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private int price;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
